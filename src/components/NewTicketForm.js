4a518d0d3b2aa065f5935073ff29cd46ed3b16f8
Jake Edgar <jakeedgar1012@gmail.com>
2022-06-27 23:46:14 -0700

    update ticket form

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(event) {
+  event.preventDefault();
+  props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <textarea 
+          className='input-text'
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <textarea
+          className='input-text'
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          className='input-text'
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button className='submit-ticket-button' type='submit'>Submit</button>
+      </form>
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
